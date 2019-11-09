4e3547826d02fdbd6794abebc9e3c6d0f2401328
ankit <mauryaankit1996@gmail.com>
2019-11-09 14:17:55 +0530

    initial commit

---
+++ src/Controller/HelloWorldController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Drupal\hello_world\Controller;
+
+use Drupal\Core\Database\Database;
+use Drupal\Core\Url;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\Core\Database\Query\PagerSelectExtender;
+
+
+class HelloWorldController extends ControllerBase {
+  // Implements hello funtion.
+  public function hello(){
+    return array(
+      '#title'=>'Hello World!',
+      '#markup'=>'Hello World!',
+    );
+ }
+
+
+  public function content() {
+    return [
+      '#theme' => 'hello_world',
+      '#test_var' => $this->t('Test Value'),
+    ];
+}
+
+  // Implements display record in table.
+  public function display() {
+    //create table header
+    $header_table = array(
+      'id'=>    t('SrNo'),
+      'image' => t('Upload')
+      'name' => t('Name'),
+      'email' => t('Email'),
+      'password' => t('Password'),
+      'dob' => t('DOB'),
+      'address' => t('Address'),
+      'number' => t('Number'),
+      'delete' => t('Delete'),
+      'edit' => t('Edit'),
+      );
+    //select records from table
+    $query = \Drupal::database()->select('hello_form', 'm');
+    $query->fields('m', ['id','image','name','email','password','dob','address','number']);
+    $pager = $query->extend('Drupal\Core\Database\Query\PagerSelectExtender')->limit(10);
+    $results = $pager->execute()->fetchAll();
+
+    //$results = $query->execute()->fetchAll();
+    $rows = [];
+    foreach($results as $data) {
+      $delete = Url::fromUserInput('/hello/delete/'.$data->id);
+      $edit   = Url::fromUserInput('/hello-form?num='.$data->id);
+      $rows[] = array(
+        'id' =>$data->id,
+        'image' => $data->image,
+        'name' => $data->name,
+        'email' => $data->email,
+        'password' => $data->password,
+        'dob' => $data->dob,
+        'address' => $data->address,
+        'number' => $data->number,
+         \Drupal::l('Delete', $delete),
+         \Drupal::l('Edit', $edit),
+      );
+
+    }
+    //display data in site
+    $form['table'] = [
+      '#type' => 'table',
+      '#header' => $header_table,
+      '#rows' => $rows,
+      '#empty' => t('No users found'),
+    ];
+
+     //pager
+     $form['pager'] = array(
+      '#type' => 'pager'
+    );
+    return $form;
+  }
+
+  // Implements delete.
+
+  function delete($id) {
+    $result = \Drupal::database()->delete('hello_form')
+      ->condition('id', $id)
+      ->execute();
+    return new RedirectResponse(\Drupal::url('hello_world.display_table_controller_display'));
+
+  }
+}
+
+?>
+
---
+++ src/Form/HelloWorldForm.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Drupal\hello_world\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Database\Database;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\Core\Render\Element;
+
+class HelloWorldForm extends FormBase
+{
+
+  public function getFormId()
+  {
+    return 'hello-form';
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $conn = Database::getConnection();
+
+     $record = array();
+    if (isset($_GET['num'])) {
+        $query = $conn->select('hello_form', 'm')
+            ->condition('id', $_GET['num'])
+            ->fields('m');
+        $record = $query->execute()->fetchAssoc();
+    }
+     //$form['#attributes']['enctype'] = 'multipart/form-data';
+     $form['image'] = array(
+      '#type' => 'file',
+      '#title' => t('Upload your file'),
+      //'#required' => true,
+     //'#upload_location' => 'public://images/'
+      '#default_value' => (isset($record['image']) && $_GET['num']) ? $record['image']:'',
+    );
+    $form['name'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Candidate Name:'),
+      '#required' => TRUE,
+      '#default_value' => (isset($record['name']) && $_GET['num']) ? $record['name']:'',
+      );
+    $form['email'] = array(
+     '#type' => 'email',
+     '#title' => t('Candidate Email:'),
+     '#required' => TRUE,
+     '#default_value' => (isset($record['email']) && $_GET['num']) ? $record['email']:'',
+     );
+    $form['password'] = array(
+      '#type' => 'password',
+      '#title' => t('Password:'),
+      '#required' => TRUE,
+       '#default_value' => (isset($record['password']) && $_GET['num']) ? $record['password']:'',
+      );
+     $form['dob'] = array (
+      '#type' => 'textfield',
+      '#title' => t('AGE:'),
+      '#default_value' => (isset($record['dob']) && $_GET['num']) ? $record['dob']:'',
+    );
+     $form['address'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Address:'),
+       '#default_value' => (isset($record['address']) && $_GET['num']) ? $record['address']:'',
+      );
+     $form['number'] = array(
+      '#type' => 'number',
+      '#title' => t('Contact number:'),
+       '#default_value' => (isset($record['number']) && $_GET['num']) ? $record['number']:'',
+      );
+
+     $form['submit'] = [
+        '#type' => 'submit',
+        '#value' => 'save',
+        //'#value' => t('Submit'),
+    ];
+    return $form;
+  }
+
+  public function hello_world_validate($form, &$form_state) {
+  // YOUR CUSTOM VALIDATION CODE GOES HERE
+    $value = $form_state->('email');
+    if($value == !\Drupal::service('email.validator')->isValid($value))){
+     $form_state->setErrorByname('email',t('The email address %mail is not valid.',array('%mail'=>$value)));
+
+
+   //if (!valid_email_address($email)) {
+     // form_set_error('submitted][email_address', t('The email address appears to be invalid.'));
+   }
+ }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+
+
+$field=$form_state->getValues();
+   $image=$field['image'];
+   $name=$field['name'];
+   $email=$field['email'];
+   $password=$field['password'];
+   $dob=$field['dob'];
+   $address=$field['address'];
+   $number=$field['number'];
+
+
+  if (isset($_GET['num'])) {
+
+          $field  = array(
+              'image'   => $image,
+              'name'   => $name,
+              'email'   => $email,
+              'password'   => $password,
+              'dob'   => $dob,
+              'address'   => $address,
+              'number'   => $number,
+              );
+          $conn = Database::getConnection();
+          $conn->update('hello_form')
+              ->fields($field)
+              ->condition('id', $_GET['num'])
+              ->execute();
+          drupal_set_message("succesfully updated");
+
+    $form_state->setRedirect('hello_world.display_table_controller_display');
+      }
+       else
+       {
+           $conn = Database::getConnection();
+           $conn->insert('hello_form')->fields(
+           array(
+            'image' => $form_state->getValue('image'),
+            'name' => $form_state->getValue('name'),
+            'email' => $form_state->getValue('email'),
+            'password' => $form_state->getValue('password'),
+            'dob' => $form_state->getValue('dob'),
+            'address' => $form_state->getValue('address'),
+            'number' => $form_state->getValue('number'),
+            )
+          )
+          ->execute();
+          drupal_set_message("succesfully saved");
+
+
+           $response = new RedirectResponse(\Drupal::url('hello_world.display_table_controller_display'));
+           $response->send();
+
+   }
+  }
+
+}
+
+
+?>
